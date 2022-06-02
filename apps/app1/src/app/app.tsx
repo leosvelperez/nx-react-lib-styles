58321797775d072cdc182242898fbc1439e75b49
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-06-02 11:50:31 +0100

    fix lib import

--- apps/app1/src/app/app.tsx
+++ apps/app1/src/app/app.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
-import { Input } from '@rio-nx/input';
+import { Input } from '@my-org/input';
 
 export function App() {
   return (
